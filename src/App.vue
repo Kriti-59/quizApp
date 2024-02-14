de0e20f796108e090dce3f8bb7a1bddf9559c80e
Kriti-59 <s556160@nwmissouri.edu>
2024-02-14 13:43:57 -0600

    Vue.js setup and basic layout of the app

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+
+  <div class = "container"> 
+  <header>
+    <h1> Quizzes</h1>
+    <input type="text" placeholder="Search for quiz..">
+     </header>
+    </div>
+</template>
+
+
+<style scoped>
+.container {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+
+header{
+  margin-bottom: 10px;
+  margin-top: 30px;
+  display: flex;
+  align-items: center;
+}
+
+header h1{
+  font-weight: bold;
+  margin-right: 40px;
+  font-size: xx-large;
+}
+
+header input{
+  border: none;
+  background-color: hsla(120, 100%, 25%, 0.3);
+  padding: 15px;
+  border-radius: 5px;
+
+}
+
+</style>
