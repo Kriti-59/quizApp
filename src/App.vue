7c1414c70d2bd0989bff5ea5190d3f3b56fb3ab0
Kriti-59 <s556160@nwmissouri.edu>
2024-02-27 14:50:26 -0600

    create Card component and use Props to handle data between components

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup>
 
 import q from "./data/quizzes.json";
-
-import {ref, watch} from "vue"
+import {ref, watch} from "vue";
+import Card from "./components/Card.vue"
 
 
 const search = ref("")
@@ -10,11 +10,9 @@ const quizzes = ref(q)
 
 watch(search, () => {
   quizzes.value = q.filter(quiz => quiz.name.toLowerCase().includes(search.value.toLowerCase()))
-
 })
 
 </script>
-
 
 <template>
 
@@ -24,13 +22,7 @@ watch(search, () => {
       <input type="text" v-model.trim="search" placeholder="Search for quiz...">
     </header>
     <div class="card-container">
-    <div v-for = "quiz in quizzes" :key = "quiz.id" class="card">
-      <img :src="quiz.img" alt="">
-      <div class="card-text">
-        <h2>{{ quiz.name }}</h2>
-        <p>{{ quiz.questions.length }} questions </p>
-      </div>
-    </div>
+      <Card v-for= "quiz in quizzes" :key="quiz.id" :quiz="quiz"/>
     </div>
 
 </div>
@@ -69,28 +61,5 @@ header input{
   flex-wrap: wrap;
 }
 
-
-.card{
-  width: 310px;
-  overflow: hidden;
-  border-radius: 2%;
-  box-shadow: 1px 1px 10px rgba (0, 0, 0, 1);
-  margin-bottom: 35px;
-  margin-right: 20px;
-  cursor: pointer;
-}
-
-.card img {
-  width: 100%;
-  height: 200px;
-  margin: none;
-}
-.card .card text{
-  padding: 0 5px;
-}
-
-.card .card-text h2{
-  font-weight: bold;
-}
 
 </style>
